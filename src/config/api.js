11f6a752187d36b1b37c3b94607e1947c671fab0
Towew <doublezx321@gmail.com>
2022-04-19 16:08:43 +0700

    Adding Var

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: process.env.SERVER_URL || 'https://dumbmerch-b32-server.herokuapp.com/api/v1/' || 'http://localhost:5000/api/v1/',
+  baseURL: process.env.REACT_APP_SERVER_URL || 'https://dumbmerch-b32-server.herokuapp.com/api/v1/' || 'http://localhost:5000/api/v1/',
 });
 
 export const setAuthToken = (token) => {
